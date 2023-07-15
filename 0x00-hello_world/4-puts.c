2111ec00752b55b1e65021e479fad56aeefac2af
smushibi <samutachika@gmail.com>
2023-07-15 14:37:17 -0700

    my first c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - No arguments 7 entry point
+ *
+ * Returns:0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
